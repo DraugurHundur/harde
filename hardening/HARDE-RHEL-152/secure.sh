0d76e0488a649c33b8c9607cda5ba3f4e7a4cb83
ghostdog <github@chevelus.org>
2021-04-19 22:43:17 +0200

    Fixes HARDE-RHEL-152

--- hardening/HARDE-RHEL-152/secure.sh
+++ hardening/HARDE-RHEL-152/secure.sh
@@ -1,3 +1,5 @@
 echo "HARDE-RHEL-152 : Authentification des utilisateurs exécutant sudo"
-grep -q -E "^[^#].*NOPASSWD" /etc/sudoers  && echo "Directive NOPASSWD à supprimer du fichier /etc/sudoers"
+if grep -q -E "^[^#].*NOPASSWD" /etc/sudoers; then 
+    echo "Directive NOPASSWD à supprimer du fichier /etc/sudoers"
+fi
 # Fin: HARDE-RHEL-152
